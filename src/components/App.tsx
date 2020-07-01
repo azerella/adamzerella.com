0926a7761e76647589afce8ad889a2d9d6f0a8b4
Adam Zerella <hi@adamzerella.com>
2020-07-02 00:02:47 +0930

    Changing thing saround

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,32 @@
 import { h, Fragment } from 'preact';
 
-import Header from './Header';
 import Footer from './Footer';
 
-import './App.scss';
+import IconTwitter from '../icons/twitter';
+import IconGitHub from '../icons/github';
+import IconLinkedin from '../icons/linkedin';
+import IconEmail from '../icons/email';
 
-export interface Props {}
+import './App.scss';
 
 export default function App() {
 	return (
 		<Fragment>
-			<Header 
-				text="Adam Zerella"
-				subHeading="Software engineer from Adelaide, Australia"
-			/>
-			<ul>
-				<li><a href="#" alt="todo">twitter</a></li>
-				<li><a href="#" alt="todo">github</a></li>
-				<li><a href="#" alt="todo">linkedin</a></li>
-				<li><a href="#" alt="todo">email</a></li>
-			</ul>
+			<main>
+				<h1>Hey, I'm Adam</h1>
+				<div class="card">
+					<blockquote>
+						I am a Software Engineer that specialises in modern, scalable web applications.
+						<p>I leverage open-source technologies, automation and JavaScript to solve problems.</p>
+					</blockquote>
+				</div>
+				<ul>
+					<li><a rel="noopener noreferrer" href="https://twitter.com/adamzerella"><IconTwitter className="icon" /></a></li>
+					<li><a rel="noopener noreferrer" href="https://github.com/adamzerella"><IconGitHub className="icon" /></a></li>
+					<li><a rel="noopener noreferrer" href="https://www.linkedin.com/in/adam-zerella-8803ab128/"><IconLinkedin className="icon" /></a></li>
+					<li><a rel="noopener noreferrer" href="mailto:hi@adamzerella.com"><IconEmail className="icon" /></a></li>
+				</ul>
+			</main>
 			<Footer />
 		</Fragment>
 	);

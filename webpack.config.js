d95f9f6e6bfe54121df357c433d622cf35d87f48
Adam Zerella <hi@adamzerella.com>
2020-07-24 15:44:01 +0930

    Add favicon to build/ output

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,7 @@ module.exports = {
 				{
 					inject: true,
 					template: path.resolve(__dirname, 'src/index.html'),
+					favicon: path.resolve(__dirname, 'src/assets/favicon.ico'),
 				},
 				isProd
 					? {

f2186ee1619caccda691b20f88fa6779c88906e4
Adam Zerella <hi@adamzerella.com>
2020-07-31 22:33:32 +0930

    Utilise flex

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,6 @@ import { h, Fragment } from 'preact';
 
 import Footer from './Footer';
 
-import IconTwitter from '../icons/twitter';
-import IconGitHub from '../icons/github';
-import IconLinkedin from '../icons/linkedin';
-import IconEmail from '../icons/email';
-
 import './App.scss';
 
 export default function App() {
@@ -15,17 +10,8 @@ export default function App() {
 			<main>
 				<h1>Hey, I'm Adam</h1>
 				<div class="card">
-					<blockquote>
-						I am a Software Engineer that specialises in modern, scalable web applications.
-						<p>I leverage open-source technologies, automation and JavaScript to solve problems.</p>
-					</blockquote>
+					<blockquote>I'm a Software Engineer that specialises in modern, scalable web applications.</blockquote>
 				</div>
-				<ul>
-					<li><a rel="noopener noreferrer" href="https://twitter.com/adamzerella"><IconTwitter className="icon" /></a></li>
-					<li><a rel="noopener noreferrer" href="https://github.com/adamzerella"><IconGitHub className="icon" /></a></li>
-					<li><a rel="noopener noreferrer" href="https://www.linkedin.com/in/adam-zerella-8803ab128/"><IconLinkedin className="icon" /></a></li>
-					<li><a rel="noopener noreferrer" href="mailto:hi@adamzerella.com"><IconEmail className="icon" /></a></li>
-				</ul>
 			</main>
 			<Footer />
 		</Fragment>

c67afe8fdb6de3992ea5164ebcaf999f3a485eb2
Adam Zerella <hi@adamzerella.com>
2020-07-31 22:42:57 +0930

    Nicer looking mail icon

--- src/icons/email.tsx
+++ src/icons/email.tsx
@@ -14,7 +14,7 @@ export default function Email(props: Props) {
         role="img"
         viewBox="0 0 24 24">
         <title>Email icon</title>
-        <path fill={props.color} d="M24 4.5v15c0 .85-.65 1.5-1.5 1.5H21V7.387l-9 6.463-9-6.463V21H1.5C.649 21 0 20.35 0 19.5v-15c0-.425.162-.8.431-1.068C.7 3.16 1.076 3 1.5 3H2l10 7.25L22 3h.5c.425 0 .8.162 1.069.432.27.268.431.643.431 1.068z"/>
+        <path fill={props.color} d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
     </svg>
     );
 }
